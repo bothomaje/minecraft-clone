d8da805eaf517a5e6f9246a2cfbae4c2e51603a6
bothomaje <bothomaje@gmail.com>
2026-06-07 12:57:31 +0200

    feat: add player camera and basic keyboard controls for movement
    * add new player class for first-person POV camera.
    * implements pointer controls for toggling between 1st and 3rd person pov.
    * implments basic keyboard controls for moving around (no collision detection)

--- scripts/ui.js
+++ scripts/ui.js
@@ -1,7 +1,7 @@
 import { GUI } from 'three/addons/libs/lil-gui.module.min.js';
 import { blocks, resources } from './blocks';
 
-export function createUI(world) {
+export function createUI(world, player) {
     const gui = new GUI();
 
     const actions = {
@@ -14,11 +14,15 @@ export function createUI(world) {
         }
     };
 
-    gui.add(world.size, 'width', 8, 128, 1).name('Width');
-    gui.add(world.size, 'height', 8, 64, 1).name('Height');
     gui.add(actions, 'fullscreen').name('Toggle Full Screen');
 
+    const playerFolder = gui.addFolder('Player');
+    playerFolder.add(player, 'maxSpeed', 1, 20).name('Max Speed');
+    playerFolder.add(player.cameraHelper, 'visible').name('Show Camera Helper');
+
     const terrainFolder = gui.addFolder('Terrain');
+    terrainFolder.add(world.size, 'width', 8, 128, 1).name('Width');
+    terrainFolder.add(world.size, 'height', 8, 64, 1).name('Height');
     terrainFolder.add(world.params, 'seed', 1, 10000).name('Seed');
     terrainFolder.add(world.params.terrain, 'scale', 10, 100).name('Scale');
     terrainFolder.add(world.params.terrain, 'magnitude', 0, 1).name('Magnitude');
