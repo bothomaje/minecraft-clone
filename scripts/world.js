d7b27a6e5a399b4d6b3fe22b4814fa34a777489d
Botho Maje <bothomaje@gmail.com>
2026-09-10 13:45:11 +0200

    feat: adds save/reload functionality

--- scripts/world.js
+++ scripts/world.js
@@ -43,13 +43,48 @@ export class World extends THREE.Group {
     constructor(seed = 0) {
         super();
         this.seed = seed;
+
+        document.addEventListener('keydown', (event) => {
+            switch (event.code) {
+                case 'F1':
+                    this.save();
+                    break;
+                case 'F2':
+                    this.load();
+                    break;
+            }
+        });
+    }
+
+    /**
+     * Saves the world data to local storage
+     */
+    save() {
+        localStorage.setItem('minecraft_params', JSON.stringify(this.params));
+        localStorage.setItem('minecraft_data', JSON.stringify(this.dataStore.data));
+        document.getElementById('status').innerHTML = 'Game saved';
+        setTimeout(() => document.getElementById('status').innerHTML = '', 3000);
+    }
+
+    /**
+     * Loads the game from disk
+     */
+    load() {
+        this.params = JSON.parse(localStorage.getItem('minecraft_params'));
+        this.dataStore.data = JSON.parse(localStorage.getItem('minecraft_data'));
+        document.getElementById('status').innerHTML = 'Game loaded';
+        setTimeout(() => document.getElementById('status').innerHTML = '', 3000);
+        this.generate();
     }
 
     /**
      * Regenerate the world data model and the meshes
      */
-    generate() {
-        this.dataStore.clear();
+    generate(clearCache = false) {
+        if (clearCache) {
+            this.dataStore.clear();
+        }
+
         this.disposeChunks();
 
         for (let x = -this.drawDistance; x <= this.drawDistance; x++) {
